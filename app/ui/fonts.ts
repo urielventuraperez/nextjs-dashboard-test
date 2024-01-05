2cd7c7824ce36e9018898aa3e10fee37cefb3fbd
Uriel Ventura <venturaperez.10101991@gmail.com>
2024-01-05 15:44:08 -0600

    init commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Montserrat, Lusitana } from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+export const lusitana = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
